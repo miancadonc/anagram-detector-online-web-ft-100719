ab77a369726391990ac8d4d389f36904ccf7ee46
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-21 00:37:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,19 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word
+  attr_accessor :word, :matches
   
   def initialize(word)
     @word = word
+    @matches = []
   end
   
   def arrays(word, tests)
     
   end
   
-  def match(word)
-    []
+  def match(testers)
+    testers.each do |tester|
+      @matches << tester if 
+    end
   end
 end
