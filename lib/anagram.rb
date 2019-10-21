05c3f4ca6402e96098608021f24c96b6c01e9789
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-21 00:53:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :matches
+  
+  def initialize(word)
+    @word = word
+    @matches = []
+  end
+  
+  def match(testers)
+    testers.each do |tester|
+      @matches << tester if @word.chars.sort.join == tester.chars.sort.join
+    end
+    @matches
+  end
+end
